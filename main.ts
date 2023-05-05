4f08c63e6805908ba9db633ff8266ab3f8da948e
juliobeti <132546465+juliobeti@users.noreply.github.com>
2023-05-05 12:30:23 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+let b = 0
+basic.forever(function () {
+    b = 225 - input.lightLevel()
+    led.setBrightness(b)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
